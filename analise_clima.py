12c64284561494ca30588c5b00a090a9400998e2
Alex Souza <alexdesouza3071@gmail.com>
2026-05-16 18:12:46 -0300

    Requisição e checagem de erro

--- analise_clima.py
+++ analise_clima.py
@@ -10,3 +10,19 @@ url = (
     "&hourly=temperature_2m,relative_humidity_2m,precipitation"
     "&forecast_days=7"
 )
+
+#Requisição e checagem de erro
+resposta = requests.get(url)
+
+if resposta.status_code == 200:
+    print("dados acessados com sucesso!")
+    print(resposta)
+
+else:
+    print("Erro ao acessar a API:", resposta.status_code)
+    exit()
+
+dados_json = resposta.json()
+
+print("\n Chaves principais do JSON:")
+print(dados_json.keys())
