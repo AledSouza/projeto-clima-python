0e6e0b03aa5add953d61f421d03e86e594de62c8
Alex Souza <alexdesouza3071@gmail.com>
2026-05-16 20:14:53 -0300

    Implementação do analise_clima com seu respectivos gráficos

--- analise_clima.py
+++ analise_clima.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 
 url = (
@@ -24,5 +25,70 @@ else:
 
 dados_json = resposta.json()
 
+
 print("\n Chaves principais do JSON:")
 print(dados_json.keys())
+
+dados_horarios = dados_json["hourly"]
+df = pd.DataFrame(dados_horarios)
+# print(df)
+
+print("\n Primeiras linhas do Dataframe:")
+# print(df.head())
+
+df["time"] = pd.to_datetime(df["time"])
+df["data"] = df["time"].dt.date
+
+print("\nResumo estatítico")
+print(df.describe())
+
+#Acumulo diario utilizando groupby
+
+df_diario = df.groupby("data").agg({
+    "temperature_2m" : "mean",
+    "relative_humidity_2m": "mean",
+    "precipitation" : "sum"
+}).reset_index()
+
+print("\n Dados agrupados por dia:")
+print(df_diario)
+
+#temperatura média por dia
+
+plt.figure(figsize=(10,5))
+plt.bar(df_diario["data"].astype(str), df_diario["temperature_2m"])
+plt.title("Temperatura Média por dia - Rio de Janeiro")
+plt.xlabel("Data")
+plt.ylabel(("Temperatura média (°C)"))
+plt.xticks(rotation=45)
+
+plt.grid(axis="y", linestyle="--", alpha=0.5)
+plt.tight_layout()
+os.makedirs("output", exist_ok=True)
+plt.savefig("output/grafico_barras_temperatura.png")
+plt.show()
+
+#gráfico de pizza
+
+plt.figure(figsize=(7,7))
+plt.pie(
+    df_diario["precipitation"],
+    labels=df_diario["data"].astype(str),
+    autopct="%1.1f%%"
+    
+    
+)
+plt.title("Distribuição da precipitação diaria:")
+plt.tight_layout()
+plt.savefig("output/grafico_pizza_chuva.png")
+plt.show()
+
+plt.figure(figsize=(8, 5))
+plt.scatter(df["temperature_2m"], df["relative_humidity_2m"])
+plt.title("Relação entre temperatura e umidade")
+plt.xlabel("Temperatura (°C)")
+plt.ylabel("Umidade relativa (%)")
+plt.tight_layout()
+plt.savefig("output/grafico_dispersao_temperatura_umidade.png")
+plt.show()
+
